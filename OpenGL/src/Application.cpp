5efcc9aaa45b639cb9ca678bbbfb7108dab91609
Kali <24emal@gmail.com>
2023-08-27 17:13:00 +0200

    create an array to hold the vertex positions and the select the buffer to draw into

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -28,17 +28,24 @@ int main(void)
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
+    float positions[6] = {
+        -0.5f, -0.5f,
+         0.0f,  0.5f,
+         0.5f, -0.5f
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer); // generate a buffer
+    glBindBuffer(GL_ARRAY_BUFFER, buffer); // select (bind) the buffer
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW); // create the buffer object with a specific size
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 3); // render primitives from array data
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
